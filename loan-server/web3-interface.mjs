a6c1cb4c2424caa36ec4280e0d3abfebc616f62a
Venkata Mahesh Polumuri <venkat2552004@gmail.com>
2024-02-27 20:34:49 +0530

    Add user related files

--- loan-server/web3-interface.mjs
+++ loan-server/web3-interface.mjs
@@ -1,6 +1,7 @@
 import Web3 from 'web3';
 import dotenv from 'dotenv';
-import contract from './build/contracts/LoanContract.json' assert { type: "json" };
+import loan_contract from './build/contracts/LoanContract.json' assert { type: "json" };
+import users_contract from './build/contracts/Users.json' assert { type: "json" };
 
 dotenv.config();
 
@@ -19,9 +20,20 @@ await web3.eth.net.isListening()
 // Adding the account I consider as loan lender to the wallet
 const account = web3.eth.accounts.wallet.add(process.env.PRIVATE_KEY); 
 
+async function getUserDetails(userAddress){
+        const contractInstance = new web3.eth.Contract(users_contract.abi, process.env.USERS_CONTRACT_ADDRESS);
+        await contractInstance.methods.getUserDetails(userAddress).call().then((response) => {
+                        console.log("User Details : ",response);
+                })
+                .catch((error) => {
+                        console.error('Failed to get the user details:', error);
+                }
+        );
+}
+
 //Function to send the loan amount to the client (amouunt, clientAddress as parameters) received from the frontend
 async function sendLoanAmount(amount, clientAddress){
-        const contractInstance = new web3.eth.Contract(contract.abi, process.env.CONTRACT_ADDRESS);
+        const contractInstance = new web3.eth.Contract(loan_contract.abi, process.env.CONTRACT_ADDRESS);
         await contractInstance.methods.sendLoan(amount, clientAddress).send({ from: account[0].address })
                 .then(async (response) => {
                         const tx = {
@@ -42,7 +54,9 @@ async function sendLoanAmount(amount, clientAddress){
 // export { sendLoanAmount };
 
 
-sendLoanAmount(1, '0x2eD26987bf37A162EaaA3D4c3Ee2D56ccdE54e92');
+// sendLoanAmount(1, '0x2eD26987bf37A162EaaA3D4c3Ee2D56ccdE54e92');
+
+//getUserDetails('0x2eD26987bf37A162EaaA3D4c3Ee2D56ccdE54e92');
 
 
 
